4c93b1f8deca18200bf2cea24ac3c9ab222fb3aa
siamangaja <siamangaja@gmail.com>
2021-12-28 15:31:39 +0700

    first commit

--- resources/views/news-single.blade.php
+++ resources/views/news-single.blade.php
@@ -7,11 +7,10 @@
     <section class="section section--first section--bg" data-bg="/img/section/section-bg.jpg">
         <div class="container">
             <div class="row">
-                <!-- section title -->
                 <div class="col-12 col-sm-10 offset-sm-1 col-lg-8 offset-lg-2 col-xl-6 offset-xl-3">
                     <h2 class="section__title section__title--white">News</h2>
+                    <p class="section__text section__text--white"><span class="breadcrumb"><a href="{{route('frontpage')}}">Home</a> / <a href="{{route('news')}}">News</a> / {{ $data->title }}</span></p>
                 </div>
-                <!-- end section title -->
             </div>
         </div>
     </section>
@@ -23,7 +22,6 @@
             <div class="row">
                 <div class="col-12 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
                     <div class="article">
-
                         <!-- article content -->
                         <div class="article__content">
                             <span class="article__date">{{ $data->created_at }}</span>
@@ -32,7 +30,6 @@
                             <p>{!! $data->content !!}</p>
                         </div>
                         <!-- end article content -->
-
                     </div>
                 </div>
             </div>
@@ -41,3 +38,13 @@
     <!-- end detail news -->
 
 @stop
+
+<style type="text/css">
+    .breadcrumb {
+        font-size: 14px;
+    }
+    .breadcrumb a{
+        color: #ccc;
+        font-weight: 500;
+    }
+</style>
